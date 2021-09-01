7a8fc013a3e3c5fe6443dc97da809a55ffb039c9
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2021-09-01 11:21:47 +0000

    Test compressed files integrity after download

--- preprocessing/download_fastq.sh
+++ preprocessing/download_fastq.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+FASTQ_DIR=raw/fastq/
+
 while read LINE
 do
 	FTP=$(echo "$LINE" | cut -f33)
-	echo "Downloading $(echo \"$FTP\" | cut -d/ -f8)..."
-	wget --no-clobber -P raw/fastq/ "$FTP"
+	echo "Downloading $(echo $FTP | cut -d/ -f8)..."
+	wget --no-clobber -P $FASTQ_DIR $FTP
+done < raw/E-MTAB-2262.sdrf.txt
+
+while read LINE
+do
+	FTP=$(echo "$LINE" | cut -f33)
+	FILE=$(echo $FTP | cut -d/ -f8)
+	gzip -vt $FASTQ_DIR/$FILE
 done < raw/E-MTAB-2262.sdrf.txt
