335ac1860d38bf04d86b4b35d09a0d619fef2902
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2021-09-01 12:33:41 +0000

    fixup e509d32

--- preprocessing/rna-seq-paired-end-workflow.sh
+++ preprocessing/rna-seq-paired-end-workflow.sh
@@ -3,7 +3,6 @@
 set -ex
 
 WORKFLOW_URL="https://gitlab.com/intelliseq/workflows/-/raw/dev/src/main/wdl/pipelines/rna-seq-paired-end/latest/rna-seq-paired-end.wdl" 
-OUT_DIR=data/rna-seq-paired-end-output/
 
 
 cromwell run $WORKFLOW_URL \
