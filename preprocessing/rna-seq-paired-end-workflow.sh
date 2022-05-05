6ab56e586435e9ae854c1aaa3ce1d2134682e4b9
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2022-05-05 12:15:40 +0000

    Specify git rev for a workflow

--- preprocessing/rna-seq-paired-end-workflow.sh
+++ preprocessing/rna-seq-paired-end-workflow.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-WORKFLOW_URL="https://gitlab.com/intelliseq/workflows/-/raw/dev/src/main/wdl/pipelines/rna-seq-paired-end/latest/rna-seq-paired-end.wdl" 
+rev=2d06f7feea5511dd578d356194fa26a90754eaf0
+WORKFLOW_URL="https://gitlab.com/intelliseq/workflows/-/raw/${rev}\
+/src/main/wdl/pipelines/rna-seq-paired-end/latest/rna-seq-paired-end.wdl"
 
 
 cromwell run $WORKFLOW_URL \
